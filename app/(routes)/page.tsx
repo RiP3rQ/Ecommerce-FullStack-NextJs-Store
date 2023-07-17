fbff31bd6fe3dbf0f25b8fd03a3669e2c13a73a6
RiP3rQ <rafalpompa2000@gmail.com>
2023-07-17 15:29:40 +0200

    featured products + main page UI

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("70c3cd0a-3c08-4a4d-9517-266e11097585");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ components/navbar-actions.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Button from "@/components/ui/button";
+
+const NavbarActions = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const router = useRouter();
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
+        <ShoppingBag size={20} color="white" />
+        <span className="ml-2 text-sm font-medium text-white">0</span>
+      </Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
